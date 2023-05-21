41ed56ab3a659bc6fffeea3adea32cbf6649f125
Manuch Sadri <colonquebang@hotmail.com>
2023-05-20 20:06:15 -0700

    adds kludge delete_user() feature. needs refactoring.

--- automation.py
+++ automation.py
@@ -29,6 +29,7 @@ def task_selection(console):
         "2": "Delete a user",
         "3": "Sort documents",
         "4": "Parse logs",
+        "5": "TBD",
     }
 
     while True:
@@ -36,7 +37,8 @@ def task_selection(console):
         user_task = Prompt.ask(f"[cyan]1.[/cyan] {task_desc.get('1')}\n"
                                f"[cyan]2.[/cyan] {task_desc.get('2')}\n"
                                f"[cyan]3.[/cyan] {task_desc.get('3')}\n"
-                               f"[cyan]4.[/cyan] {task_desc.get('4')}\n\n"
+                               f"[cyan]4.[/cyan] {task_desc.get('4')}\n"
+                               f"[cyan]5.[/cyan] {task_desc.get('5')}\n\n"
                                f"Enter the # for your selection")
 
         # task_confirm = Prompt.ask(f"\nYou selected: [yellow]{user_task}. {task_desc.get(user_task)}[/yellow]\nConfirm "
@@ -56,9 +58,9 @@ def create_folder(console, folder_name=str()):
         while not name_test:
             os.system('clear')
             folder_name = Prompt.ask("[chartreuse3]Make a new folder[/chartreuse3]\nWhat do you want to name your new "
-                                     "folder?\n\nThe default name = [cyan]temp[/cyan]")
+                                     "folder?\n\nThe default name = [cyan]doodah[/cyan]")
             if folder_name == "":
-                folder_name = "temp"
+                folder_name = "doodah"
 
             name_test = os.path.exists(folder_name)
 
@@ -97,17 +99,52 @@ def create_folder(console, folder_name=str()):
 
 
 def delete_user(console):
-    os.system('clear')
-    console.print(f"[red]So you want to delete a user? Does that make you feel powerful?[/red]")
 
-    users = sorted(os.listdir("./user-docs"))
-    table = Table(title=f"Users n [cyan]user-docs[/cyan]")
-    table.add_column("User Name")
-    for user in users:
-        table.add_row(user)
-    console.print(table)
+    path = "./user-docs"
+    user_to_del = str()
+    user_path = str()
 
+    while True:
+        os.system('clear')
+        console.print(f"[red]So you want to delete a user? Does that make you feel powerful?[/red]\n")
 
+        users = sorted(os.listdir(path))
+        table = Table(title=f"[cyan]All Users")
+        table.add_column("Row")
+        table.add_column("User Name")
+        table.add_column("# of Files")
+        for user in users:
+            user_path = path + "/" + user
+            table.add_row(str(users.index(user) + 1), user, str(len(os.listdir(user_path))))
+
+        console.print(table)
+        selected_row = Prompt.ask(f"\nWhich unforunate soul do you wish to delete?\nEnter the [cyan]row #[/cyan] to be deleted")
+        if selected_row.isnumeric():
+            selected_row = int(selected_row)
+            if 0 < selected_row <= len(users):
+                user_to_del = users[selected_row - 1]
+                break
+        else:
+            Prompt.ask(f"\n[yellow]{selected_row}[/yellow], is not a valid entry. Enter [cyan]any[/cyan] key to try again")
+
+    # create deleted_users folder if it doesn't already exist
+    deleted_path = "deleted_users"
+    name_test = os.path.exists(deleted_path)
+    if not name_test:
+        create_folder(console, deleted_path)
+
+    # create the user folder for the deleted user in the deleted_users folder
+    deleted_path = "deleted_users"
+    name_test = os.path.exists(deleted_path + "/" + user_to_del)
+    if not name_test:
+        create_folder(console, deleted_path + "/" + user_to_del)
+
+    # move the user files from user-docs to deleted-users
+    user_path = path + "/" + user_to_del
+    deleted_user_path = deleted_path + "/" + user_to_del
+    user_files = os.listdir(user_path)
+    for file in user_files:
+        shutil.move(user_path + "/" + file, deleted_user_path + "/" + file)
 
 
 def sort_docs():
